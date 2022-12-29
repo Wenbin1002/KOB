3aee4cac3d13304b7e6c5d837e559bb4e0049fe7
Waterdance1002 <waterdance1002@gmail.com>
2022-12-29 16:52:44 +0800

    user profile

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -5,11 +5,86 @@
 </template>
 
 <script>
-    import ContentField from '../../../components/ContentField.vue'
+import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex';
 
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3030/user/bot/add/",
+        //     type: "POST",
+        //     data: {
+        //         title: "Bot的标题",
+        //         description : "Bot的描述",
+        //         content: "Bot的代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+
+    //    $.ajax({
+    //         url: "http://127.0.0.1:3030/user/bot/remove/",
+    //         type: "POST",
+    //         data: {
+    //             bot_id: 10,
+    //         },
+    //         headers: {
+    //             Authorization: "Bearer " + store.state.user.token,
+    //         },
+    //         success(resp) {
+    //             console.log(resp);
+    //         },
+    //         error(resp) {
+    //             console.log(resp);
+    //         }
+    //     })
+
+        //     $.ajax({
+        //     url: "http://127.0.0.1:3030/user/bot/update/",
+        //     type: "POST",
+        //     data: {
+        //         bot_id: 7,
+        //         title: "Bot的标题ddd",
+        //         description : "Bot的描述ddd",
+        //         content: "Bot的代码ddd",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        $.ajax({
+            url: "http://127.0.0.1:3030/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
     }
 }
 </script>
